2df3862e23502368dd3a9d9c94ba89309f4cfcd5
Maria Inês Silva <misilva73@gmail.com>
2025-06-18 09:54:07 +0200

    Add bal_builder unit tests

--- tests/test_bal_builder.py
+++ tests/test_bal_builder.py
@@ -17,6 +17,8 @@ from bal_builder import (
     build_contract_nonce_diffs_from_state,
 )
 
+## Auxiliary functions
+
 
 def single_transaction_trace(contract_address) -> List[dict]:
     pre_dict = {
@@ -38,6 +40,9 @@ def single_transaction_trace(contract_address) -> List[dict]:
         }
     ]
     return trace_output
+
+
+## Tests
 
 
 def test_storage_diff_single_slot_update():
@@ -77,4 +82,58 @@ def test_balance_diff_single_balance_update():
     # Correct tx index
     assert balance_diff_list[0].changes[0].tx_index == 0
     # Correct tx delta
-    int.from_bytes(balance_diff_list[0].changes[0].delta) == 1
+    assert int.from_bytes(balance_diff_list[0].changes[0].delta) == 1
+
+
+def test_code_diff_single_code_update():
+    contract_address = "0x1385cfe2ac49c92c28e63e51f9fcdcc06f93ed09"
+    trace_output = single_transaction_trace(contract_address)
+    trace_output[0]["result"]["post"][contract_address]["code"] = "0x62"
+    _, code_diff_list = get_code_diff_from_block(trace_output)
+    # Single contract
+    assert len(code_diff_list) == 1
+    # Correct contract address
+    assert code_diff_list[0].address == to_canonical_address(contract_address)
+    # Single code change
+    assert len(code_diff_list[0].changes) == 1
+    # Correct tx index
+    assert code_diff_list[0].changes[0].tx_index == 0
+    # Correct new code
+    assert code_diff_list[0].changes[0].new_code.hex() == "62"
+
+
+def test_contract_nonce_diffs_eoa_nonce_update():
+    contract_address = "0x1385cfe2ac49c92c28e63e51f9fcdcc06f93ed09"
+    trace_output = single_transaction_trace(contract_address)
+    del trace_output[0]["result"]["pre"][contract_address]["code"]
+    del trace_output[0]["result"]["post"][contract_address]["code"]
+    trace_output[0]["result"]["post"][contract_address]["nonce"] = 2
+    # no nonce diffs
+    _, nonce_diffs_list = build_contract_nonce_diffs_from_state(trace_output)
+    assert len(nonce_diffs_list) == 0
+
+
+def test_contract_nonce_diffs_contract_nonce_update():
+    contract_address = "0x1385cfe2ac49c92c28e63e51f9fcdcc06f93ed09"
+    trace_output = single_transaction_trace(contract_address)
+    trace_output[0]["result"]["post"][contract_address]["nonce"] = 2
+    # no nonce diffs
+    _, nonce_diffs_list = build_contract_nonce_diffs_from_state(trace_output)
+    assert len(nonce_diffs_list) == 1
+
+
+def test_no_update():
+    contract_address = "0x1385cfe2ac49c92c28e63e51f9fcdcc06f93ed09"
+    trace_output = single_transaction_trace(contract_address)
+    # no access updates
+    _, account_access_list = get_storage_diff_from_block(trace_output)
+    assert len(account_access_list) == 0
+    # no balance diffs
+    _, balance_diff_list = get_balance_diff_from_block(trace_output)
+    assert len(balance_diff_list) == 0
+    # no code diffs
+    _, code_diff_list = get_code_diff_from_block(trace_output)
+    assert len(code_diff_list) == 0
+    # no nonce diffs
+    _, nonce_diffs_list = build_contract_nonce_diffs_from_state(trace_output)
+    assert len(nonce_diffs_list) == 0
